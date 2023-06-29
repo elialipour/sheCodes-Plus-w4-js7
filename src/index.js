44f36ec2ab3c2f95f54fb2fd2693333c9a6be842
Elham Alipour <136172033+elialipour@users.noreply.github.com>
2023-06-29 16:24:19 +0330

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// Challenge 1
+// Log li with id special
+let specialCountry = document.querySelector("li#special");
+console.log(specialCountry);
+
+// Challenge 2
+// Log all li with class of country
+let country = document.querySelectorAll(".country");
+console.log(country);
+// Challenge 3
+// Add class special to the li with id special
+specialCountry.classList.add("special");
+
+// Challenge 4
+// In your code, replace the content of the element with id special
+// by your favorite country (other than Portugal of course)
+special.innerHTML = "Iran";
